7afa6d4f94d6cf64ed024add1cc73de1c652b795
Enciyo <enc1y0@hotmail.com>
2018-10-17 17:53:10 +0300

    Runtime Permission

--- app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
+++ app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
@@ -1,37 +1,55 @@
 package t.timer.mobilliumtrellotask
 
-import android.content.Context
+import android.Manifest.permission.*
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val PREFS_FILE = ".com.sharedpreferences"
-    val KEY_NAME = "NAME"
-    val KEY_AGE = "AGE"
-    val KEY_JOB = "JOB"
-
+    companion object {
+        val TAG: String = MainActivity.javaClass.simpleName
+        val REQUEST_CODE: Int = 101
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val preferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE)
-        val editor = preferences.edit()
-
-        btnCommit.setOnClickListener {
-            editor.putString(KEY_NAME, edtName.text.toString())
-            editor.putString(KEY_AGE, edtAge.text.toString())
-            editor.putString(KEY_JOB, edtJob.text.toString())
-            editor.apply()
+        btnNetworkPermission.setOnClickListener {
+            ActivityCompat.requestPermissions(this, arrayOf(CAMERA), REQUEST_CODE)
         }
 
-        txvResult.text = preferences.getString(KEY_NAME, "Don't save name")+ "\n" +
-                preferences.getString(KEY_AGE, "Don't save age")+ "\n" +
-                preferences.getString(KEY_JOB,"Don't save job")
+        btnCheckNetwork.setOnClickListener {
+            val permission = ContextCompat.checkSelfPermission(this, CAMERA)
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                txvStateNetwork.text = "Don't have Permission"
+            } else if (permission == PackageManager.PERMISSION_GRANTED) {
+                txvStateNetwork.text = "Already has permission"
+            }
+        }
 
 
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission has been denied by user", Toast.LENGTH_SHORT).show()
+                    Log.i(TAG, "Permission has been denied by user")
+                } else {
+                    Toast.makeText(this, "Permission has been granted by user", Toast.LENGTH_SHORT).show()
+                    Log.i(TAG, "Permission has been granted by user")
+                }
+            }
+        }
     }
 
 
