60ec2472b05f83db0ca81bb9193060bccd428fae
Enciyo <enc1y0@hotmail.com>
2018-10-18 23:37:32 +0300

    Layouts-Widget-CardView

--- app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
+++ app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
@@ -14,42 +14,14 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         val TAG: String = MainActivity.javaClass.simpleName
-        val REQUEST_CODE: Int = 101
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnNetworkPermission.setOnClickListener {
-            ActivityCompat.requestPermissions(this, arrayOf(CAMERA), REQUEST_CODE)
-        }
-
-        btnCheckNetwork.setOnClickListener {
-            val permission = ContextCompat.checkSelfPermission(this, CAMERA)
-            if (permission != PackageManager.PERMISSION_GRANTED) {
-                txvStateNetwork.text = "Don't have Permission"
-            } else if (permission == PackageManager.PERMISSION_GRANTED) {
-                txvStateNetwork.text = "Already has permission"
-            }
-        }
 
 
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            REQUEST_CODE -> {
-
-                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permission has been denied by user", Toast.LENGTH_SHORT).show()
-                    Log.i(TAG, "Permission has been denied by user")
-                } else {
-                    Toast.makeText(this, "Permission has been granted by user", Toast.LENGTH_SHORT).show()
-                    Log.i(TAG, "Permission has been granted by user")
-                }
-            }
-        }
     }
 
 
