05ef255b2d40c2cea2e0d0096b6fceb2dffcfdf4
Enciyo <enc1y0@hotmail.com>
2018-10-19 15:36:40 +0300

    Layouts-Widget-CardView

--- app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
+++ app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
@@ -7,19 +7,45 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     companion object {
         val TAG: String = MainActivity.javaClass.simpleName
     }
 
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Log.i(TAG, "onNothingSelected")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+        Log.i("OnItemSelected", position.toString())
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val spinner: Spinner = findViewById(R.id.spinner)
+        spinner.onItemSelectedListener = this
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.Spinner_array,
+            R.layout.support_simple_spinner_dropdown_item
+        ).also { arrayAdapter ->
+
+            arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+            spinner.adapter = arrayAdapter
+        }
 
 
     }
