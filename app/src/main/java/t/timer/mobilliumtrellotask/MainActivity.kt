3ed28ccdb9a9a97d5116d5dae0a1c9e6fe775ff1
Enciyo <enc1y0@hotmail.com>
2018-10-19 16:09:04 +0300

    Spinner-CheckBox-SeekBar

--- app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
+++ app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
@@ -2,40 +2,37 @@ package t.timer.mobilliumtrellotask
 
 import android.Manifest.permission.*
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,SeekBar.OnSeekBarChangeListener {
 
     companion object {
         val TAG: String = MainActivity.javaClass.simpleName
     }
 
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        Log.i(TAG, "onNothingSelected")
-    }
 
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
-        Log.i("OnItemSelected", position.toString())
-    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        spinner.onItemSelectedListener = this
+        //SeekBar
+        sBar.setOnSeekBarChangeListener(this)
+        sBar.progress = 40
+        sBar.max=60
+        ///
 
+        //Spinner
+        spinner.onItemSelectedListener = this
         ArrayAdapter.createFromResource(
             this,
             R.array.Spinner_array,
@@ -45,9 +42,52 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
             spinner.adapter = arrayAdapter
         }
+        ///
+
+        //CheckBox
+        cBox.setOnClickListener {
+            checkBoxControl()
+        }
+        ///
 
 
     }
+    //CheckBox
+    private fun checkBoxControl() {
+        if(cBox.isChecked){
+            txvState.text="Checkbox Enable"
+        }
+        else {
+            txvState.text="CheckBox Disable"
+        }
+    }
+    ///
+
+    //Spinner
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        txvState.text=seekBar?.progress.toString()
+    }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Log.i(TAG, "onNothingSelected")
+    }
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Log.i("OnItemSelected", position.toString())
+        txvState.text=parent?.getItemAtPosition(position).toString()
+    }
+    ///
+
+    //SeekBar
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        txvState.text="onStartTrackingTouch"
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+        txvState.text="onStopTrackingTouch"
+    }
+    ///
+
+
+
 
 
 }
