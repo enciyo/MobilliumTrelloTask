bd79c42b366d99e3b886875de7ba6c7744122b08
Enciyo <enc1y0@hotmail.com>
2018-10-21 01:19:19 +0300

    WebView

--- app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
+++ app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
@@ -3,6 +3,7 @@ package t.timer.mobilliumtrellotask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,17 +18,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i(TAG,"onCreate")
+        Log.i(TAG, "onCreate")
 
 
-        var adapter:listAdapter =listAdapter(this,Color.colors)
-
-         listView.adapter=adapter
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.google.com.tr")
 
 
     }
-
-
 
 
 }
