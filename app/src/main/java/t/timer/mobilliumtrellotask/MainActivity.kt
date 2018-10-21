93a5e73e71f06ac03b06d932312934314a7f2b71
Enciyo <enc1y0@hotmail.com>
2018-10-21 21:48:40 +0300

    RecyclerView

--- app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
+++ app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.widget.GridLayout
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "onCreate")
 
         val layoutManager=LinearLayoutManager(this)
-        layoutManager.orientation=LinearLayout.VERTICAL
+        layoutManager.orientation=GridLayout.VERTICAL
         rcyclerView.layoutManager=layoutManager
         val adapter:Adapter = Adapter(this,Repos.repo)
         rcyclerView.adapter=adapter
