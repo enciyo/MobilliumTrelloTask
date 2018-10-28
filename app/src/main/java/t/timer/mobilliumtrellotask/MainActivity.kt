d464fe0f9b8a158a4f970de11721e6d2db559ddc
Enciyo <enc1y0@hotmail.com>
2018-10-28 18:46:24 +0300

    Toolbar

--- app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
+++ app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
@@ -1,8 +1,13 @@
 package t.timer.mobilliumtrellotask
 
+import android.graphics.Color
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,12 +21,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar.title="Simple Toolbar"
+        toolbar.setTitleTextColor(Color.WHITE)
+        setSupportActionBar(toolbar)
 
-        btn.setOnClickListener {
-            val runner: asynTask = asynTask(textView.text,textView,this,relativeLayout)
-            runner.execute(textView.text.toString())
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_item,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId) {
+            R.id.appbar_add -> Snackbar.make(relativeLayout,"Add on Click",Snackbar.LENGTH_SHORT).show()
+            R.id.appbar_play -> Snackbar.make(relativeLayout,"Play on Click",Snackbar.LENGTH_SHORT).show()
+            R.id.appbar_share-> Snackbar.make(relativeLayout,"Share on Click",Snackbar.LENGTH_SHORT).show()
         }
-
+        return true
     }
 
 
