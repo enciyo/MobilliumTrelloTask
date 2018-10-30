dd5b6e63336655354caee0073bc6c972ad25a2c8
Enciyo <enc1y0@hotmail.com>
2018-10-30 16:41:09 +0300

    Navigation Architecture Component

--- app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
+++ app/src/main/java/t/timer/mobilliumtrellotask/MainActivity.kt
@@ -1,13 +1,11 @@
 package t.timer.mobilliumtrellotask
 
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,18 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toolbar.title = "Simple Toolbar"
-        toolbar.setTitleTextColor(Color.WHITE)
-        setSupportActionBar(toolbar)
-        NavigationUI.setupActionBarWithNavController(this,NavHostFragment.findNavController(myHostFragment))
+
+        NavigationUI.setupActionBarWithNavController(
+            this,
+            NavHostFragment.findNavController(myHostFragment)
+        )//set appbar
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-       return findNavController(R.id.myHostFragment).navigateUp()
+        return findNavController(R.id.myHostFragment).navigateUp()
     }
-
-
 
 
 }
